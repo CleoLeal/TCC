bb09d3ec6264db95c2730148622090b6ff7feb73
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-09-30 12:26:07 -0300

    Atualização 30/09/22
    window.open("") serve para abrir outro HTML

--- Login Page/Empresa/prosseguir.js
+++ Login Page/Empresa/prosseguir.js
@@ -3,10 +3,8 @@ let razao = document.getElementById("razaoSocial")
 let fantasia = document.getElementById("fantasia")
 let email = document.getElementById("email")
 let tel = document.getElementById("telefone")
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-})
+
 function prosseguir1()
 {
-    window.location.replace("criarContaEmpresa2")
+   window.open("criarContaEmpresa2.html");
 }
