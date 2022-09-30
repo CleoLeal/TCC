117505943ab21d3f8a77d46995e64e9b256b1470
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-09-30 10:51:52 -0300

    Atualização 30/09/2022
    Tentativa falha do onclick no botão. eu irei tentar de novo mais tarde

--- Login Page/Empresa/prosseguir.js
+++ Login Page/Empresa/prosseguir.js
@@ -1,4 +1,12 @@
+const form = document.getElementById("form");
+let razao = document.getElementById("razaoSocial")
+let fantasia = document.getElementById("fantasia")
+let email = document.getElementById("email")
+let tel = document.getElementById("telefone")
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+})
 function prosseguir1()
 {
-    window.location.href="teste.html";
+    window.location("criarContarEmpresa2.html")
 }
