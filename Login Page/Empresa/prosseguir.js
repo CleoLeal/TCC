b60ee76ec6dd55af67fe15b2761320632f2caef5
Cleo Leal <cleovleal@gmail.com>
2022-09-30 17:43:45 -0300

    Atualização 30/09/22
    O window.open("") serve para abrir uma nova janela, e não continuar na mesma. Por isso eu alterei para o window.location = ""

--- Login Page/Empresa/prosseguir.js
+++ Login Page/Empresa/prosseguir.js
@@ -1,10 +1,9 @@
-const form = document.getElementById("form");
+
 let razao = document.getElementById("razaoSocial")
 let fantasia = document.getElementById("fantasia")
 let email = document.getElementById("email")
 let tel = document.getElementById("telefone")
 
-function prosseguir1()
-{
-   window.open("criarContaEmpresa2.html");
+function prosseguir1(){
+   window.location = "./criarContaEmpresa2.html";
 }
