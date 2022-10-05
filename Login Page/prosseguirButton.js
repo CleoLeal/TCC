ffec2a1009524bb5b249fc8d6a1fb7a1230c2c1b
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-10-04 23:02:13 -0300

    Atualização 04/10/22
    Esse JS serve para os botões do do entrar e criar página, para ser levado para as páginas seguinte, como podemos observar.

---
+++ Login Page/prosseguirButton.js
@@ -0,0 +1,27 @@
+//botão criar conta da página login empresa
+function criarContaEmpresa()
+{
+   window.location = "./criarContaEmpresa1.html";
+}
+
+//botão prosseguir da página criar conta empresa 1
+function prosseguir1Empresa(){
+   window.location = "./criarContaEmpresa2.html";
+}
+//botão prosseguir da página criar conta empresa 2
+function prosseguir2Empresa(){
+   window.location = "./criarContaEmpresa3.html";
+}
+//botão criar conta da página criar conta empresa3
+function criarContaEmpresa2()
+{
+   window.location = "./loginEmpresa.html";
+}
+
+/*function criarConta (){
+   window.location = "./criarContaEmpresa.html";
+}*/
+
+function prosseguir1Cliente(){
+   window.location = "./criarContaCliente2.html";
+}
