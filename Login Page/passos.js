bc13c674aaf8907b099684651cb3c955142126ae
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-10-04 23:01:40 -0300

    Atualização 04/10/22
    Troquei eles de local para serem mais fácil de encontrar. Essas funções são os passos que tem em cima, na página de criar conta. Para ficar mais fácil para as pessoas voltarem a página passada

---
+++ Login Page/passos.js
@@ -0,0 +1,20 @@
+//botão passo, para voltar a tela criarContaEmpresa1 no html do criarContaEmpresa2
+function passo1Empresa()
+{
+    window.location = "./criarContaEmpresa1.html";
+}
+//botão passo, para voltar tela criarContaEmpresa1 tela no html do criarContaEmpresa3
+function passo11Empresa()
+{
+    window.location = "./criarContaEmpresa1.html";
+}
+//botão passo, para voltar tela criarContaEmpresa2 tela no html do criarContaEmpresa3
+function passo2Empresa()
+{
+    window.location = "./criarContaEmpresa2.html";
+}
+//botão passo, para voltar tela criarContaCliente1 tela no html do criarContaCliente2
+function passo1Cliente()
+{
+    window.location = "./criarContaCliente1.html";
+}
