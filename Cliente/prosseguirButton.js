4108c4ef5a926d6db91afb5f4bc97b473c2b7f5e
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-10-05 10:48:12 -0300

    Atualização 05/10/22
    Adicionei as funções da página de login cliente

---
+++ Cliente/prosseguirButton.js
@@ -0,0 +1,40 @@
+//botão criar conta da página login cliente
+function criarContaCliente1()
+{
+   window.location = "./criarContaCliente1.html";
+}
+
+//botão entrar como empresa na página login cliente
+function entrarEmpresa1(){
+   window.location = "./Empresa/loginEmpresa.html";
+}
+
+//botão criar conta da página login empresa
+function criarContaEmpresa()
+{
+   window.location = "./criarContaEmpresa1.html";
+}
+
+//botão prosseguir da página criar conta empresa 1
+function prosseguir1Empresa(){
+   window.location = "./criarContaEmpresa2.html";
+}
+
+//botão prosseguir da página criar conta empresa 2
+function prosseguir2Empresa(){
+   window.location = "./criarContaEmpresa3.html";
+}
+
+//botão criar conta da página criar conta empresa3
+function criarContaEmpresa2()
+{
+   window.location = "./loginEmpresa.html";
+}
+
+/*function criarConta (){
+   window.location = "./criarContaEmpresa.html";
+}*/
+
+function prosseguir1Cliente(){
+   window.location = "./criarContaCliente2.html";
+}
