872de7bc5e717941f447ff0e56e1e5685072abf1
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-10-10 17:43:23 -0300

    Comentários
    Adicionei comentários

--- js/autenticacaoEmpresa.js
+++ js/autenticacaoEmpresa.js
@@ -1,20 +1,28 @@
+//variáveis
 var email = '';
 var senha = '';
+
+//funções
 function validar()
 {
+    //pegando o valor dos inputs e colocando o valor dentro de uma variável
     email=document.getElementById('emailEmpresa').value;
     senha=document.getElementById('senhaEmpresa').value;
 
+    //fiz condições de email e senha que não existem, só para testar. como ainda não usamos bando de dados, fica complicado.
     if(email=='toquenarede@gmail.com' && senha=='tnr2022')
     {
+        //abre a home empresa
         window.location="./../Tcc-homeusuario/home_empresa.html";
     }
     else if(email=='' || senha=='')
     {
+        //abre um alerta
         alert("Está faltando Email ou Senha");
     }
     else if((email!='toquenarede@gmail.com' && senha=='tnr2022') || (email=='toquenarede@gmail.com' && senha!='tnr2022') || (email!='toquenarede@gmail.com' && senha!='tnr2022'))
     {
+        //abre um alerta
         alert("O Email ou Senha está errado");
     }
     
