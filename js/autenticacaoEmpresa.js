5b541cdc0140fcb643f479775b8a1235710c0d02
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-10-10 16:49:00 -0300

    Lógica
    Consegui fazer a lógica básica para conseguir entrar nas páginas seguintes

---
+++ js/autenticacaoEmpresa.js
@@ -0,0 +1,21 @@
+var email = '';
+var senha = '';
+function validar()
+{
+    email=document.getElementById('emailEmpresa').value;
+    senha=document.getElementById('senhaEmpresa').value;
+
+    if(email=='toquenarede@gmail.com' && senha=='tnr2022')
+    {
+        window.location="./../Tcc-homeusuario/home.html";
+    }
+    else if(email=='' || senha=='')
+    {
+        alert("Está faltando Email ou Senha");
+    }
+    else if((email!='toquenarede@gmail.com' && senha=='tnr2022') || (email=='toquenarede@gmail.com' && senha!='tnr2022') || (email!='toquenarede@gmail.com' && senha!='tnr2022'))
+    {
+        alert("O Email ou Senha está errado");
+    }
+    
+}
