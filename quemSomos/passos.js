515b7dee6b9da21751add25d259be2cc6de67bae
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-12-03 18:31:52 -0300

    Comentários
    Adicionei os comentários nesse arquivo de back-end

--- quemSomos/passos.js
+++ quemSomos/passos.js
@@ -1,8 +1,9 @@
+//avança para uma outra página
 function entrar()
 {
     window.location = "./../../toquenarede/Cliente/LoginPage.php";
 }
-
+//avança para uma outra página
 function quemSomos()
 {
     window.location= "./index.php";
