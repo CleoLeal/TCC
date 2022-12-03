802be866efc8f2ff387813e9e09255f34ecac740
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-12-03 11:13:11 -0300

    Comentários
    Adicionei os comentários nos arquivos de back-end

--- Cliente/passos.js
+++ Cliente/passos.js
@@ -1,13 +1,14 @@
+//passo1, retorna para a página criarConta
 function passo1()
 {
     window.location="./CriarContaCliente1.php";
 }
-
+//passo1, avança para a página criarConta
 function criarConta()
 {
     window.location="./CriarContaCliente1.php";
 }
-
+//passo1, avança para a página de emrpesa
 function entrarEmpresa()
 {
     window.location= "./../../toquenarede/Empresa/loginEmpresa.php"
